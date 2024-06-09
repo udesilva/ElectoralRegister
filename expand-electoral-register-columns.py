a0629dac6aeb6c623c320efffd6c437ae4eaf9d5
Uditha DeSilva <Uditha.I.DeSilva@gmail.com>
2024-06-09 01:19:30 +0100

    Refactor with logging to console and logfile

--- expand-electoral-register-columns.py
+++ expand-electoral-register-columns.py
@@ -1,9 +1,18 @@
-# Description: This script expands the electoral register columns in an unencrypted xlsx spreadsheet.
 import argparse
 import datetime
 import os
-
+import logging
 import openpyxl
+import textwrap
+
+# Expands the electoral register columns in an unencrypted xlsx spreadsheet.
+
+# Set up logging to file and console
+logging.basicConfig(filename='expand-electoral-register.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console)
 
 # Create an argument parser
 parser = argparse.ArgumentParser(description='Process an unencrypted xlsx spreadsheet.')
@@ -13,6 +22,11 @@ parser.add_argument('xlsx_path', type=str,
 # Parse the command line arguments
 args = parser.parse_args()
 
+# record the start time
+start_time = datetime.datetime.now()
+
+logging.info(f"Reading {args.xlsx_path}")
+
 # Load the decrypted xlsx spreadsheet
 with open(args.xlsx_path, 'rb') as f:
     workbook = openpyxl.load_workbook(args.xlsx_path)
@@ -21,8 +35,7 @@ with open(args.xlsx_path, 'rb') as f:
 new_workbook = openpyxl.Workbook()
 new_sheet = new_workbook.active
 
-# record the start time
-start_time = datetime.datetime.now()
+logging.info("Processing the spreadsheet")
 
 # Iterate over the rows in the first sheet of the encrypted xlsx spreadsheet
 sheet = workbook.active
@@ -30,49 +43,60 @@ header = None
 for row in sheet.iter_rows():
     if header is None:
         header = row
-        # create index by using the values of the cells in the header row to give the column number by name
+        # create index by using the values of the cells in the header row to 
+        # give the column number by name
         index = {cell.value: i for i, cell in enumerate(header)}
         _markers = index['Elector Markers']
         _postcode = index['PostCode']
         _lastrow = len(header) - 1
-        new_sheet.append([cell.value for cell in row])  # Copy the header row to the new sheet
+        # Copy the header row to the new sheet
+        new_sheet.append([cell.value for cell in row])
         continue
 
-    row_ = [str(i.value).replace('\xa0', ' ')
-            if i.value is not None else '' for i in row]
+    row_val = [str(i.value).replace('\xa0', ' ')
+               if i.value is not None else '' for i in row]
     changed = False
 
-    # Exclude "B" and "G" Elector markers for EU citizens who cannot vote in UK General Elections
-    if row_[_markers] in ["B", "G"]:
+    # Exclude "B" and "G" Elector markers for EU citizens who cannot vote in 
+    # UK General Elections
+    if row_val[_markers] in ["B", "G"]:
         continue
 
     # Get the postcode fields
-    postcode = row_[_postcode]
+    postcode = row_val[_postcode]
     if postcode == '':
         continue
 
-    if postcode in row_[_postcode+1:]:
+    if postcode in row_val[_postcode+1:]:
+        while row_val[_lastrow] != postcode:
+            row_val.insert(_postcode+1, '')
 
-        while row_[_lastrow] != postcode:
-            row_.insert(_postcode+1, '')
-
-        row_ = row_[:_lastrow+1]
+        row_val = row_val[:_lastrow+1]
         changed = True
+
     # Copy the row to the new sheet
-    new_sheet.append(row_)
+    new_sheet.append(row_val)
 
 # Save the results to a new xlsx file
+
 # Get the original filename
 original_filename = os.path.basename(args.xlsx_path)
 
-# Add _xp to the basename
+# Add _filtered to the basename
 new_filename = os.path.splitext(original_filename)[0] + '_filtered.xlsx'
+
+logging.info(f"Saving to {new_filename}")
 
 # Save the results to the new xlsx file
 new_workbook.save(new_filename)
 
+logging.info("Done.")
+
 # record the end time
 end_time = datetime.datetime.now()
 
-# Print the time taken
-print(f"Time taken: {end_time - start_time} seconds")
+# Log the time taken
+time_diff_seconds = (end_time - start_time).total_seconds()
+rounded_seconds = round(time_diff_seconds, 1)
+
+logging.info(f"Time taken: {rounded_seconds} seconds")
